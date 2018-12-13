18975e3bbd5d9aea87783075e7dd98ddc3ed1608
Iyanuoluwa Opadotun <iyanuoluwa.opadotun@interswitchgroup.com>
2018-12-13 17:01:28 +0100

    Committed all

---
+++ url-shortener/src/test/java/com/interswitch/urlshortener/service/UrlGenServiceTest.java
@@ -0,0 +1,16 @@
+package com.interswitch.urlshortener.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UrlGenServiceTest {
+
+    @Test
+    public void createUrl() {
+    }
+
+    @Test
+    public void getOriginalUrl() {
+    }
+}
